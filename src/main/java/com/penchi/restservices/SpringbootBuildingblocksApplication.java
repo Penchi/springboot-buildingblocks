a6c3ee008a9f25f53d05153afb401a9049879ff4
Penchi <epn9009@gmail.com>
2020-01-06 18:24:38 +0530

    Hellow World Base

--- src/main/java/com/penchi/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/penchi/restservices/SpringbootBuildingblocksApplication.java
@@ -1,4 +1,4 @@
-package com.penchi.restservices.springbootbuildingblocks;
+package com.penchi.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
