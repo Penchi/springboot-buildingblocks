a1362490b24f462f0a6e47e1ad77ae1b8920a6ae
Penchi <epn9009@gmail.com>
2020-01-06 14:24:53 +0530

    First Commit

---
+++ src/main/java/com/penchi/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.penchi.restservices.springbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+}
