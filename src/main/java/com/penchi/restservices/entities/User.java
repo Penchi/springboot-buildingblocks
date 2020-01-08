f838303e4aac02c81325d535901853bf64b54b9a
Penchi <epn9009@gmail.com>
2020-01-08 16:56:49 +0530

    SpringBoot JPA OneToMany

--- src/main/java/com/penchi/restservices/entities/User.java
+++ src/main/java/com/penchi/restservices/entities/User.java
@@ -1,9 +1,12 @@
 package com.penchi.restservices.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -14,31 +17,34 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	@NotEmpty(message = "User Name is Mandatory, Plese Provide the User Name.")
 	@Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
 	private String userName;
-	
+
 	@Size(min = 2, message = "First Name should have at least 2 characters.")
 	@Column(name = "FIRST_NAME", length = 50, nullable = false)
 	private String firstName;
-	
+
 	@Column(name = "LAST_NAME", length = 50, nullable = false)
 	private String lastName;
-	
+
 	@Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
 	private String email;
-	
+
 	@Column(name = "ROLE", length = 50, nullable = false)
 	private String role;
-	
+
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	private String ssn;
+
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders;
 
 	public User() {
 	}
 
-	public User(Long id, String userName, String firstName, String lastName, String email,String role, String ssn) {
+	public User(Long id, String userName, String firstName, String lastName, String email, String role, String ssn) {
 		this.id = id;
 		this.userName = userName;
 		this.firstName = firstName;
@@ -80,7 +86,6 @@ public class User {
 		this.lastName = lastName;
 	}
 
-	
 	public String getEmail() {
 		return email;
 	}
@@ -104,8 +109,13 @@ public class User {
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
-	
-	
-	
-	
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 insert into user values (101, 'Apple.A@gmail.com', 'Apple', 'A', 'Manager', 'ssn101', 'Apple A');
 insert into user values (102, 'Ball.B@gmail.com', 'Ball', 'B', 'TechLead', 'ssn102', 'Ball B');
 insert into user values (103, 'Cat.C@gmail.com', 'Cat', 'C', 'Associate', 'ssn103', 'Cat C');
+
+insert into orders (orderid, orderdescription, user_id) values (2001, 'Order-1', 101);
+insert into orders (orderid, orderdescription, user_id) values (2002, 'Order-2', 101);
+insert into orders (orderid, orderdescription, user_id) values (2003, 'Order-3', 101);
+insert into orders (orderid, orderdescription, user_id) values (2004, 'Order-1', 102);
+insert into orders (orderid, orderdescription, user_id) values (2005, 'Order-2', 102);
